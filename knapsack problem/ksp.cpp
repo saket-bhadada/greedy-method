83db2e913566210bd85754361201b9df0fd14c88
saket-bhadada <saketbhadada@gmail.com>
2026-01-04 00:23:16 +0530

    ksp

---
+++ knapsack problem/ksp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+int main()
+{
+    int q[5];
+    int p[5];
+    int w[5];
+    int cpacity;
+}
+int profit(int (&q)[5], int (&p)[5], int (&w)[5], int capacity)
+{
+    int pw[5];
+}
