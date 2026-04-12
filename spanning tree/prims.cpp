0da076a04080b7199e3db99d5fb8281dc9aaea90
saket-bhadada <saketbhadada@gmail.com>
2026-04-12 15:58:56 +0530

    working on prims algo

--- spanning tree/prims.cpp
+++ spanning tree/prims.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-#include <array>
+// #include <array>
+#include <vector>
+void prims(std::vector<std::vector<int>> graph)
+{
+    int n = graph.size();
+    std::vector<int> parent(n);
+    std::vector<int> cost(n);
+
+    // checking if the edge is already included in the mst or not
+    std::vector<bool> ver(n, false);
+}
 
 int main()
 {
