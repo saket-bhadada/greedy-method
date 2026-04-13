a12e7df8959a3e13f9d5da0cf684414a317fb6ca
saket-bhadada <saketbhadada@gmail.com>
2026-04-13 20:59:59 +0530

    completing job sequancing

--- jobsequance.cpp
+++ jobsequance.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> profit(std::vector<int> jobs,std::vector<int> profit,std::vector<int> deadlines,int limit)
+std::vector<int> jobSequence(std::vector<int> &jobs,std::vector<int> &profit,std::vector<int> &deadlines,int limit)
 {
     // sorting on the basis of max profit
     int n = profit.size();
@@ -23,22 +23,38 @@ std::vector<int> profit(std::vector<int> jobs,std::vector<int> profit,std::vecto
     {
         if(deadline<deadlines[i])
         {
-            deadline = deadlines[i]
+            deadline = deadlines[i];
         }
     }
-    std::vector<int> sequence;
+    std::vector<int> sequence(deadline,0);
+    std::cout << "sequence: " << sequence.size() << std::endl;
+    std::vector<int> result;
     // sequencing the jobs on the basis of deadline
     for(int i=0;i<n;i++)
     {
-        if(sequence[i]!=0)
+        // Try to place the job in the latest available slot up to its deadline
+        for(int j=deadlines[i]-1;j>=0;j--)
         {
-            for(int j=i;j>0;j--)
+            if(sequence[j]==0)
             {
-                if(sequence[j]==0)
-                {
-                    sequence.push_back()
-                }
+                sequence[j] = jobs[i];
+                result.push_back(jobs[i]);
+                break;
             }
         }
     }
+    return result;
+}
+
+int main()
+{
+    std::vector<int> jobs = {1,2,3,4,5};
+    std::vector<int> profit = {20,15,10,5,1};
+    std::vector<int> deadlines = {2,2,1,3,3};
+    std::vector<int> result = jobSequence(jobs,profit,deadlines,5);
+    for(int i=0;i<result.size();i++)
+    {
+        std::cout << result[i] << " ";
+    }
+    return 0;
 }
